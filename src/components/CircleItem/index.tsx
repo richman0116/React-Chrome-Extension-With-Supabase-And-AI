ef0e22a1371dbfb024b5e7b2cc536501317cdbc7
richman0116 <maria8partee@gmail.com>
2023-11-29 22:33:12 +0900

    Update circles text style

--- src/components/CircleItem/index.tsx
+++ src/components/CircleItem/index.tsx
@@ -10,10 +10,10 @@ const CircleItem = ({ circle }: CircleItemInterface) => {
       href={`https://edenzero.vercel.app/circle/${circle.id}`}
       rel="noreferrer"
       target="_blank"
-      className="circles-item px-4 my-2 h-20 transition-transform transform hover:scale-105 hover:cursor-pointer flex gap-5 items-center group"
+      className="circles-item px-4 my-2 h-20 transition-transform transform hover:cursor-pointer flex gap-5 items-center group"
     >
       <div className="rounded-full min-w-[80px] h-20 bg-gray-300 group-hover:bg-gray-400" />
-      <div className="flex flex-col justify-between gap-2 text-sm group-hover:text-base text-gray-700 group-hover:text-gray-900">
+      <div className="flex flex-col justify-between gap-2 text-sm text-gray-700 group-hover:text-gray-900">
         <p className="font-semibold">{circle.name}</p>
         <p className="text-ellipsis line-clamp-2">{circle.description}</p>
       </div>

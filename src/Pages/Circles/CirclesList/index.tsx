cd18cb32d294d6add426d1810616c94ca3353293
richman0116 <maria8partee@gmail.com>
2023-12-07 10:05:17 +0900

    change claim Button text

--- src/Pages/Circles/CirclesList/index.tsx
+++ src/Pages/Circles/CirclesList/index.tsx
@@ -113,7 +113,7 @@ const CirclList = ({ setPageStatus, url, circles, setCircles }: ICircleList) => 
               onClick={() => setPageStatus(circlePageStatus.CLAIM_CIRCLE)}
               className=" px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             >
-              Claim Circle
+              Claim For My Circles
             </button>
           </div>
         ) : null}

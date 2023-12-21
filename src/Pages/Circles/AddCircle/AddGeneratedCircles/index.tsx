7e1c2461025f5bdc59e6c161d71030f638605955
richman0116 <maria8partee@gmail.com>
2023-12-21 14:14:37 -0600

    only check for response length to determine if the response is successful

--- src/Pages/Circles/AddCircle/AddGeneratedCircles/index.tsx
+++ src/Pages/Circles/AddCircle/AddGeneratedCircles/index.tsx
@@ -32,14 +32,14 @@ const AddGeneratedCircles = ({ setPageStatus, setAddPageStatus, url }: AddGenera
                   {action: 'getGeneratedCircles', pageUrl: url, pageContent: limitedWords},
                   (res2) => {
                     console.log('Generated circles with limited words: ', res2)
-                    if (res2[0] !== "I'm sorry") {
+                    if (res2.length >= 5) {
                       setCircles(res2)
                     }
                     setIsLoading(false)
                   }
                 )
               } else {
-                if (res1[0] !== "I'm sorry") {
+                if (res1.length >= 5) {
                   setCircles(res1)
                 }
                 setIsLoading(false)
